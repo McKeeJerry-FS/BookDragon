31dfb774fdd10c8ad4dcd53c1c12a6cac28dd7d5
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-07-30 08:20:10 -0400

    extended the Identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookDragon.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookDragon.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
