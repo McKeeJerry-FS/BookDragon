05dff495886176998412c2b7d5c06b75caacc7a3
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2025-08-10 20:54:24 -0400

    Updated book type

---
+++ Data/Migrations/20250810232800_006_AddBookTypeToBooks.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookDragon.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _006_AddBookTypeToBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookType",
+                table: "Books",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookType",
+                table: "Books");
+        }
+    }
+}
